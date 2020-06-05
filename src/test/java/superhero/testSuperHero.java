928b294c27cf3eecb65564a9f29fecae9e56b3a9
deeplygends <peeren.pro@gmail.com>
2020-06-05 14:27:29 +0200

    add singleton check test

--- src/test/java/superhero/testSuperHero.java
+++ src/test/java/superhero/testSuperHero.java
@@ -15,7 +15,6 @@ import java.util.logging.Logger;
 
 class testSuperHero
 {
-    private SuperHero superHer1;
     private BadGuy badGuy1;
     private BadGuy badGuy2;
     private static Logger log =java.util.logging.Logger.getLogger("SuperHeroTest");
@@ -28,7 +27,6 @@ class testSuperHero
     public void setUp() // throws java.lang.Exception
     {
         log.info("Before statement");
-        superHer1 = SuperHero.getInstance();
         badGuy1 = new BadGuy();
         badGuy2 = new BadGuy(1000);
     }
@@ -46,21 +44,28 @@ class testSuperHero
     @Test
     public void testEquals()
     {
+        SuperHero superHer1 = SuperHero.getInstance();
+        SuperHero superHer2 = SuperHero.getInstance();
         assertTrue(!superHer1.equals(null));
         assertEquals(superHer1, superHer1);
         assertEquals(superHer1, SuperHero.getInstance());
+        assertEquals(superHer1, superHer2);
         assertTrue(superHer1.getIdentity().equals("Spiderman"));
+
     }
     @Test
     public void testWorkOut()
     {
+        SuperHero superHer1 = SuperHero.getInstance();
+        var strength = superHer1.getStrength();
         log.info("Test WorkOut Start");
-        assertEquals(65, superHer1.workOut());
+        assertEquals(strength + 15, superHer1.workOut());
     }
 
     @Test
     public void testFightBadGuy()
     {
+        SuperHero superHer1 = SuperHero.getInstance();
         log.info("Test FightBadGuy");
         assertEquals(true, superHer1.fightAllBadGuys());
     }
@@ -68,28 +73,33 @@ class testSuperHero
     @Test
     public void testSetIndentity()
     {
+        SuperHero superHer1 = SuperHero.getInstance();
         superHer1.setIdentity("Moi");
         assertEquals("Moi", superHer1.getIdentity());
     }
     @Test
     public void testGetIndentity()
     {
+        SuperHero superHer1 = SuperHero.getInstance();
         assertEquals("Spiderman", superHer1.getIdentity());
     }
     @Test
     public void testGetStrength()
     {
+        SuperHero superHer1 = SuperHero.getInstance();
         assertEquals(50, superHer1.getStrength());
     }
     @Test
     public void testSetStrength()
     {
+        SuperHero superHer1 = SuperHero.getInstance();
         superHer1.setStrength(50);
         assertEquals(50, superHer1.getStrength());
     }
     @Test
     public void testAddBadGuy()
     {
+        SuperHero superHer1 = SuperHero.getInstance();
         BadGuy bg = new BadGuy(100);
         superHer1.addBadGuy(bg);
         assertFalse(superHer1.fightAllBadGuys());
